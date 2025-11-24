cd3382bad729b9ccdf51ee362735693011dc2003
mukulsingh24 <rmks1004@gmail.com>
2025-11-24 23:05:29 +0530

    backend server initialized

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 
-const app = express;
-app.request(cors)
+const app = express();
+app.use(cors())
 const port = 3000
 
 app.listen(port,()=>{
